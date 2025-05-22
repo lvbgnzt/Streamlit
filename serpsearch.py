a79ee380c11be77c386491b1ea50922f4f8d49a8
plsvb <vonbrandtlaurin@gmail.com>
2025-05-22 10:11:39 +0100

    sql modul

--- serpsearch.py
+++ serpsearch.py
@@ -14,9 +14,6 @@ def search_serpapi(query, serp_api_key, location):
 
 from firecrawl import FirecrawlApp
 
-def fetch_markdown_from_url(url: str, firecrawl_api_key: str) -> str:
+def fetch_markdown_from_url(url: str, firecrawl_api_key: str):
     app = FirecrawlApp(api_key=firecrawl_api_key)
-    response = app.scrape_url(url, formats=['markdown'])
-    if response.get("success") and "data" in response and "markdown" in response["data"]:
-        return response["data"]["markdown"]
-    return "Fehler beim Abrufen von Markdown."
+    return app.scrape_url(url, formats=['markdown'])
